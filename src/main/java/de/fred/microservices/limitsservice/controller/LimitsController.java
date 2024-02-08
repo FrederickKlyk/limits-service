f2447e27d45a60701c332dcd0a640b6807bbebd6
Frederick Klyk <klyk@adesso-mobile.de>
2024-02-08 11:18:44 +0100

    config over properties

--- src/main/java/de/fred/microservices/limitsservice/controller/LimitsController.java
+++ src/main/java/de/fred/microservices/limitsservice/controller/LimitsController.java
@@ -1,14 +1,21 @@
 package de.fred.microservices.limitsservice.controller;
 
 import de.fred.microservices.limitsservice.bean.Limits;
+import de.fred.microservices.limitsservice.configuration.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
 
-    @GetMapping
-   public Limits retrieveLimits(){
-        return new Limits(1, 1000);
-   }
+    private Configuration configuration;
+
+    public LimitsController(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
+    @GetMapping("/limits")
+    public Limits retrieveLimits() {
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
+    }
 }
